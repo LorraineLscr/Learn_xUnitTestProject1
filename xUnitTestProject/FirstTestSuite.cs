5b27161919f3b01659331ba4ad3b531adb37dcce
LorraineLscr <lorraiine76@hotmail.fr>
2022-11-03 16:49:55 +0100

    Ajoutez des fichiers projet.

---
+++ xUnitTestProject/FirstTestSuite.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace xUnitTestProject
+{
+    public class FirstTestSuite
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.True(true);
+        }
+    }
+}
